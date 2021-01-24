5f595aad7f3a0fe06e348fdeefe6d3db1f124241
Jan Soons <jsoons@gmail.com>
2021-01-24 12:24:29 +0100

    routes js

---
+++ server/src/routes.js
@@ -0,0 +1,8 @@
+module.exports = (app) => {
+    app.post('/register', (req, res) => {
+        res.send({
+            message: `Your user ${req.body.email} was registered!`
+        })
+    })
+
+}
